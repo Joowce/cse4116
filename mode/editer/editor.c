c558ca679285f5d2ed2b78fe832d4ac8be1b1d6e
joowce <joy@github.com>
2019-04-17 23:28:58 +0900

    # mode ## editor - 한 줄 초과

--- mode/editer/editor.c
+++ mode/editer/editor.c
@@ -111,7 +111,7 @@ int edit_text (int sw) {
 
     if (sw == prev_btn && sw != SW12) text[len - 1] = '\0';
 
-    if (len > LCD_MAX_BUFF) {
+    if (len >= LCD_MAX_BUFF) {
         strncpy((char *)temp, (char *)(&text[1]), len - 1);
         strncpy((char*)text, (char*)temp, len - 1);
         text[LCD_MAX_BUFF - 1] = '\0';
